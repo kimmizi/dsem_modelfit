3cfbc2e86e30793d477d20fdb291261402205050
mihaifalcusan <mihai.falcusan@student.uni-tuebingen.de>
2024-12-26 12:38:21 +0200

    added file to automatically load libraries and paralellise code on cluster cores

---
+++ src/load_libraries.R
@@ -0,0 +1,28 @@
+# Function to centralize library loading
+load_libraries <- function() {
+  # List of required libraries
+  required_packages <- c(
+    "doParallel", "foreach", "doSNOW", "future", "profvis", "Rmpi",
+    "mvtnorm", "matrixcalc", "lavaan", "blavaan", "rstan"
+  )
+  
+  # Install and load each library
+  for (pkg in required_packages) {
+    if (!require(pkg, character.only = TRUE)) {
+      message(paste("Installing missing package:", pkg))
+      install.packages(pkg, repos = "http://cran.r-project.org")
+    }
+    library(pkg, character.only = TRUE)
+  }
+  
+  # Specific configurations for certain packages
+  if ("rstan" %in% installed.packages()) {
+    rstan_options(auto_write = TRUE)
+    options(mc.cores = parallel::detectCores()) # Enable parallel processing for rstan
+  }
+  
+  message("All required libraries loaded and configured.")
+}
+
+# Call the library-loading function
+load_libraries()
